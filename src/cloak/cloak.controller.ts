a0ad9c9601a6f20732905f91451797476d5e0c02
Vadym <vadimchunihin.v@gmail.com>
2025-10-29 17:16:03 +0100

    added full swagger documentation

--- src/cloak/cloak.controller.ts
+++ src/cloak/cloak.controller.ts
@@ -1,12 +1,28 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CloakService } from './cloak.service';
 import { RequestDto } from './dto/request.dto';
-import { CheckResult } from './types/check-result.type';
+import { CheckResult, CheckResultEnum } from './types/check-result.type';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Cloak Service')
 @Controller()
 export class CloakController {
   constructor(private readonly cloakService: CloakService) {}
   @Post('check')
+  @ApiOperation({ summary: 'Check if a request is from a bot or not' })
+  @ApiBody({
+    type: RequestDto,
+    description: 'Request data including headers and IP',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns "bot" or "not bot"',
+    schema: {
+      type: 'string',
+      enum: [...CheckResultEnum],
+      example: 'bot',
+    },
+  })
   async check(@Body() body: RequestDto): Promise<CheckResult> {
     return await this.cloakService.check(body);
   }
--- src/cloak/dto/request.dto.ts
+++ src/cloak/dto/request.dto.ts
@@ -2,17 +2,30 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsIP, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class RequestDto {
-  @ApiProperty({ description: 'IPv4 of the request' })
+  @ApiProperty({
+    description: 'IPv4 of the request',
+    example: '127.0.0.1',
+    required: true,
+  })
   @IsNotEmpty({ message: 'IP is required' })
   @IsIP(4, { message: 'This field should be IPv4' })
   ip: string;
 
-  @ApiProperty({ description: 'User agent for the request' })
+  @ApiProperty({
+    description: 'User agent for the request',
+    example:
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.117 Safari/537.36',
+    required: false,
+  })
   @IsOptional()
   @IsString({ message: 'User agent for the request is invalid' })
   userAgent?: string;
 
-  @ApiProperty({ description: 'accept-language header' })
+  @ApiProperty({
+    description: 'Accept-Language header of the request',
+    example: 'en-US,en;q=0.9,fr;q=0.8',
+    required: false,
+  })
   @IsOptional()
   @IsString({ message: 'accept-language header invalid' })
   acceptLanguage?: string;
