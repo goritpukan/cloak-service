bc7a6bea3c16d5ed7a8f0474e9425a81808b94b6
Vadym <vadimchunihin.v@gmail.com>
2025-10-29 13:14:55 +0100

    add swagger, husky, mongoose and docker-compose file to the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cloak service')
+    .setDescription('Cloak service API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+  await app.listen(3000);
 }
 bootstrap();
